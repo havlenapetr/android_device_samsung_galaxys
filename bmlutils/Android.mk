b4c10104616a079a84befe7df5e020ee0a2fd970
Petr Havlena <havlenapetr@gmail.com>
2011-06-06 22:46:34 +0200

    build bmlwrite into /root/sbin

--- bmlutils/Android.mk
+++ bmlutils/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := bmlwrite
 LOCAL_MODULE_TAGS := eng
-LOCAL_MODULE_PATH := releasetools/kernel-tools
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)/sbin
 LOCAL_SRC_FILES := bmlwrite.c
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_STATIC_LIBRARIES += libstdc++ libc
