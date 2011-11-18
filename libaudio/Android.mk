66b833ec8c1f8095cd61f05fdd809e22c2bb5713
Petr Havlena <havlenapetr@gmail.com>
2011-11-18 10:14:27 +0100

    libaudio - update for ics

--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -1,5 +1,7 @@
 LOCAL_PATH:= $(call my-dir)
 
+# * * * * * * * * * * * * * * * some audio utils * * * * * * * * * * * * * * *
+
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= aplay.c alsa_pcm.c alsa_mixer.c
 LOCAL_MODULE:= aplay
@@ -21,33 +23,66 @@ LOCAL_SHARED_LIBRARIES := libc libcutils
 LOCAL_MODULE_TAGS:= debug
 include $(BUILD_EXECUTABLE)
 
+# * * * * * * * * * * * * * * * * audio driver * * * * * * * * * * * * * * * *
+
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= AudioHardware.cpp alsa_mixer.c alsa_pcm.c
-LOCAL_MODULE:= libaudio
-LOCAL_STATIC_LIBRARIES:= libaudiointerface
-LOCAL_SHARED_LIBRARIES:= libc libcutils libutils libmedia libhardware_legacy
+
+LOCAL_SHARED_LIBRARIES := \
+    libcutils \
+    libutils \
+    libmedia \
+    libhardware_legacy
+
+LOCAL_STATIC_LIBRARIES := \
+    libmedia_helper
+
+LOCAL_WHOLE_STATIC_LIBRARIES := \
+    libaudiohw_legacy
+
+LOCAL_MODULE := audio.primary.aries
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_TAGS := optional
+
 ifeq ($(BOARD_HAVE_BLUETOOTH),true)
-  LOCAL_SHARED_LIBRARIES += liba2dp
+    LOCAL_CFLAGS += \
+        -DWITH_BLUETOOTH \
+        -DWITH_A2DP
 endif
 
 ifeq ($(BOARD_HAVE_FM_RADIO),true)
-  LOCAL_CFLAGS += -DHAVE_FM_RADIO
+    LOCAL_CFLAGS += -DHAVE_FM_RADIO
 endif
 
 ifeq ($(TARGET_SIMULATOR),true)
- LOCAL_LDLIBS += -ldl
+   LOCAL_LDLIBS += -ldl
 else
- LOCAL_SHARED_LIBRARIES += libdl
+   LOCAL_SHARED_LIBRARIES += libdl
 endif
 
 include $(BUILD_SHARED_LIBRARY)
 
+# * * * * * * * * * * * * * * * audio policy driver * * * * * * * * * * * * * * *
+
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= AudioPolicyManager.cpp
-LOCAL_MODULE:= libaudiopolicy
-LOCAL_STATIC_LIBRARIES:= libaudiopolicybase
-LOCAL_SHARED_LIBRARIES:= libc libcutils libutils libmedia
+
+LOCAL_SHARED_LIBRARIES := \
+    libcutils \
+    libutils \
+    libmedia
+
+LOCAL_STATIC_LIBRARIES := \
+    libmedia_helper
+
+LOCAL_WHOLE_STATIC_LIBRARIES := \
+    libaudiopolicy_legacy
+
+LOCAL_MODULE := audio_policy.aries
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_TAGS := optional
+
 ifeq ($(BOARD_HAVE_BLUETOOTH),true)
-  LOCAL_CFLAGS += -DWITH_A2DP
+    LOCAL_CFLAGS += -DWITH_A2DP
 endif
 include $(BUILD_SHARED_LIBRARY)
