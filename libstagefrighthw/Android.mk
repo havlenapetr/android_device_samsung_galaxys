9439eadb2aead189ac33727c940ae86ce8aa70cd
Petr Havlena <havlenapetr@gmail.com>
2011-11-18 10:13:58 +0100

    libstagefrighthw  - update for ics

--- libstagefrighthw/Android.mk
+++ libstagefrighthw/Android.mk
@@ -2,8 +2,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-    stagefright_overlay_output.cpp \
-    SecHardwareRenderer.cpp \
     SEC_OMX_Plugin.cpp
 
 LOCAL_CFLAGS += $(PV_CFLAGS_MINUS_VISIBILITY)
