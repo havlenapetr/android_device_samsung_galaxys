2638ef4ad3bda98b6f062b9c4d3795eed7389b52
Petr Havlena <havlenapetr@gmail.com>
2011-12-25 11:41:11 +0100

    stagefright - update from crespo

--- libstagefrighthw/Android.mk
+++ libstagefrighthw/Android.mk
@@ -9,7 +9,6 @@ LOCAL_CFLAGS += $(PV_CFLAGS_MINUS_VISIBILITY)
 LOCAL_C_INCLUDES:= \
       $(TOP)/frameworks/base/include/media/stagefright/openmax \
       $(LOCAL_PATH)/../include \
-      $(LOCAL_PATH)/../liboverlay
 
 LOCAL_SHARED_LIBRARIES :=    \
         libbinder            \
@@ -17,9 +16,10 @@ LOCAL_SHARED_LIBRARIES :=    \
         libcutils            \
         libui                \
         libdl                \
-        libsurfaceflinger_client
 
 LOCAL_MODULE := libstagefrighthw
 
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
+
+
