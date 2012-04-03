af21ee4430231a7d58e2c6cf749e6d8f275fac90
Petr Havlena <havlenapetr@gmail.com>
2012-04-03 17:44:33 +0200

    releasetools: don't use global OPTIONS

--- releasetools/mksecbootimg.py
+++ releasetools/mksecbootimg.py
@@ -3,16 +3,6 @@ import os
 import sys
 import getopt
 
-class Options(object): pass
-OPTIONS = Options()
-OPTIONS.out_file = None
-OPTIONS.kernel = None
-OPTIONS.ramdisk = None
-OPTIONS.cmdline = None
-OPTIONS.recovery = None
-OPTIONS.base = None
-OPTIONS.pagesize = None
-
 def copydata(outfile, infile):
   while 1:
     data = infile.read(512)
@@ -35,6 +25,14 @@ def appendimage(outfile, infile):
   return (offset/512, length/512)
 
 def main(argv):
+  out_file = None
+  kernel = None
+  ramdisk = None
+  cmdline = None
+  recovery = None
+  base = None
+  pagesize = None
+
   try:
     opts, args = getopt.getopt(argv, "o", ["kernel=", "ramdisk=", "output=", "cmdline=", "base=", "pagesize="])
   except getopt.GetoptError, err:
@@ -43,34 +41,34 @@ def main(argv):
 
   for o, a in opts:
     if o == "--kernel":
-      OPTIONS.kernel = open(a, 'r')
+      kernel = open(a, 'r')
     elif o == "--ramdisk":
-      OPTIONS.ramdisk = open(a, 'r')
+      ramdisk = open(a, 'r')
     elif o in ("-o", "--output"):
-      OPTIONS.out_file = open(a, 'wb')
+      out_file = open(a, 'wb')
     elif o == "--cmdline":
-      OPTIONS.cmdline = a
+      cmdline = a
     elif o == "--base":
-      OPTIONS.base = a
+      base = a
     elif o == "--pagesize":
-      OPTIONS.pagesize = a
+      pagesize = a
     else:
       assert False, "unhandled option"
 
   offset_table = "\n\nBOOT_IMAGE_OFFSETS\n"
-  copydata(OPTIONS.out_file, OPTIONS.kernel)
-  table_loc = alignoffset(OPTIONS.out_file)
-  OPTIONS.out_file.write('\x00' * 512)
-  offset_table += "boot_offset=%d;boot_len=%d;" % appendimage(OPTIONS.out_file, OPTIONS.ramdisk)
+  copydata(out_file, kernel)
+  table_loc = alignoffset(out_file)
+  out_file.write('\x00' * 512)
+  offset_table += "boot_offset=%d;boot_len=%d;" % appendimage(out_file, ramdisk)
   #if recovery:
-  #  offset_table += "recovery_offset=%d;recovery_len=%d;" % appendimage(OPTIONS.out_file, recovery)
+  #  offset_table += "recovery_offset=%d;recovery_len=%d;" % appendimage(out_file, recovery)
   offset_table += "\n\n"
 
-  OPTIONS.out_file.seek(table_loc)
-  OPTIONS.out_file.write(offset_table)
-  OPTIONS.out_file.flush()
-  os.fsync(OPTIONS.out_file.fileno())
-  OPTIONS.out_file.close()
+  out_file.seek(table_loc)
+  out_file.write(offset_table)
+  out_file.flush()
+  os.fsync(out_file.fileno())
+  out_file.close()
 
 if __name__ == '__main__':
   try:
