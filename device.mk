3f581c6790c3ac21b5e302d1320787ea68afde7b
Petr Havlena <havlenapetr@gmail.com>
2012-04-24 19:20:48 +0200

    clean impl of *.rc scripts

--- device.mk
+++ device.mk
@@ -48,7 +48,6 @@ PRODUCT_COPY_FILES := \
 
 # Init files
 PRODUCT_COPY_FILES += \
-        device/samsung/galaxys/init.rc:root/init.rc \
         device/samsung/galaxys/init.aries.rc:root/init.aries.rc \
         device/samsung/galaxys/ueventd.aries.rc:root/ueventd.aries.rc \
         device/samsung/galaxys/recovery.rc:root/recovery.rc \
