d77dc78f9d3c06c445e32b6e4b3a3d680bab2826
Petr Havlena <havlenapetr@gmail.com>
2012-07-05 07:37:50 +0200

    overlay: set scan interval into framework config, property is not supported anymore

--- device.mk
+++ device.mk
@@ -187,7 +187,6 @@ PRODUCT_PROPERTY_OVERRIDES := \
 # be reachable from resources or other mechanisms.
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0 \
-    wifi.supplicant_scan_interval=20 \
     ro.telephony.ril_class=samsung \
     mobiledata.interfaces=pdp0,eth0,gprs,ppp0 \
     dalvik.vm.heapsize=32m
