426a85c3964eb992c90ee51fdce6882cbf918376
Petr Havlena <havlenapetr@gmail.com>
2012-09-02 18:17:08 +0200

    fix other stuff

--- releasetools.py
+++ releasetools.py
@@ -16,22 +16,77 @@
 """Emit commands needed for Galaxys during OTA installation
 (installing boot image and modem firmware)."""
 
-import os
+import re
 import common
+
+def FindRadio(zipfile):
+  matches = []
+  for name in zipfile.namelist():
+    if re.match(r"^RADIO/radio[.](.+[.])?img$", name):
+      matches.append(name)
+  if len(matches) > 1:
+    raise ValueError("multiple radio images in target-files zip!")
+  if matches:
+    print "using %s as modem.bin" % (matches[0],)
+    return zipfile.read(matches[0])
+  else:
+    return None
+
+def FullOTA_InstallEnd(info):
+  radio_img = FindRadio(info.input_zip)
+  if not radio_img:
+    print "no modem.bin in target_files; skipping install"
+  else:
+    common.ZipWriteStr(info.output_zip, "modem.bin", radio_img)
+    info.script.Print("Writing radio...")
+    info.script.WriteRawImage("/radio", "modem.bin")
+
+def IncrementalOTA_InstallEnd(info):
+  tf = FindRadio(info.target_zip)
+  if not tf:
+    # failed to read TARGET radio image: don't include any radio in update.
+    print "no modem.bin in target target_files; skipping install"
+  else:
+    tf = common.File("modem.bin", tf)
+
+    sf = FindRadio(info.source_zip)
+    if not sf:
+      # failed to read SOURCE radio image: include the whole target
+      # radio image.
+      tf.AddToZip(info.output_zip)
+      info.script.Print("Writing radio...")
+      info.script.WriteRawImage("/radio", tf.name)
+    else:
+      sf = common.File("modem.bin", sf)
+
+      if tf.sha1 == sf.sha1:
+        print "radio image unchanged; skipping"
+      else:
+        diff = common.Difference(tf, sf)
+        common.ComputeDifferences([diff])
+        _, _, d = diff.GetPatch()
+        if d is None or len(d) > tf.size * common.OPTIONS.patch_threshold:
+          # computing difference failed, or difference is nearly as
+          # big as the target:  simply send the target.
+          tf.AddToZip(info.output_zip)
+          info.script.Print("Writing radio...")
+          info.script.WriteRawImage("radio", tf.name)
+        else:
+          common.ZipWriteStr(info.output_zip, "modem.bin.p", d)
+          info.script.Print("Patching radio...")
+          radio_type, radio_device = common.GetTypeAndDevice(
+              "/radio", info.info_dict)
+          info.script.ApplyPatch(
+              "%s:%s:%d:%s:%d:%s" % (radio_type, radio_device,
+                                     sf.size, sf.sha1, tf.size, tf.sha1),
+              "-", tf.size, tf.sha1, sf.sha1, "modem.bin.p")
 
 def FullOTA_Assertions(info):
   devices = ["aries", "galaxys", "GT-I9000", "GT-I9000M", "GT-I9000T"]
   info.script.AssertDevices(devices)
-
-  info.script.UnpackPackageDir("firmware", "/tmp")
-  info.script.SetPermissions("/tmp/modem.bin", 0, 0, 0644)
   return True
 
 def FullOTA_WriteBootimg(info):
-  out_path = os.getenv('OUT')
-
-  info.output_zip.write(os.path.join(out_path, "modem.bin"), "firmware/modem.bin")
-
   # write boot.img with start_block
   info.script.WriteRawImage("/boot", "boot.img", 72)
   return True
